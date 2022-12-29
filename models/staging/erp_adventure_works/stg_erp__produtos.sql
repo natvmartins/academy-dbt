b24236d34a21da40233bbdee295ce81ab86cfacf
natalimaluquete1@gmail.com <natalimaluquete1@gmail.com>
2022-12-29 20:48:03 +0000

    iniciando a staging de produtos

---
+++ models/staging/erp_adventure_works/stg_erp__produtos.sql
@@ -0,0 +1,33 @@
+with
+    source_product as (
+        select 
+            cast (productid as string) as id_produto
+            , cast (productsubcategoryid as string) as id_subcategoria_produto
+            , cast (name as string) as nome_produto
+            , cast (productnumber as string) as numero_produto
+            , cast (color as string) as cor_produto
+            , cast (safetystocklevel as int) as nivel_estoque_seguranca
+            , cast (standardcost as numeric) as custo_padrao
+            , cast (listprice as numeric) as preco_de_lista
+            , cast (daystomanufacture as numeric) as dias_para_fabricar
+            , cast (productline	as string) as linha
+            , cast (class as string) as classe
+            , cast (style as string) as estilo
+            --, makeflag 
+            --, finishedgoodsflag
+            --, reorderpoint
+            --, size
+            --, sizeunitmeasurecode
+            --, weightunitmeasurecode
+            --, weight
+            --, sellstartdate
+            --, sellenddate	as string
+            --, discontinueddate	
+            --, rowguid	as string
+            --, modifieddate
+            --, productmodelid
+ 
+    )
+
+select *
+from source_product
